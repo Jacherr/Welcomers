4427b55eb8766948cb5509c3f4e168b0db1cd1f9
Jacher <jwc2002@outlook.com>
2019-12-25 22:43:36 +0000

    config example

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+    masterToken: 'THE MASTER BOT TOKEN',
+    tokens: "TOKEN1,TOKEN2,TOKEN3",
+    prefix: ';',
+    welcomeChannel: 'CHANNEL_ID',
+    logChannel: 'CHANNEL_ID',
+    welcomeGuild: 'GUILD_ID',
+    userPerms: {
+      'USER_ID': ['all'], // Gives access to all commands
+      'USER_ID': ['say', 'join', 'leave']
+    },
+    blacklistedIds: ['USER_ID', 'USER_ID']
+  }
+  
