ad659de474b94baa9e449572197fe229c9fb6ecd
Jacher <jwc2002@outlook.com>
2019-12-25 22:35:06 +0000

    re-add it

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,14 @@
 const Command = require('../types/Command')
+const { readdir } = require('fs');
+const { promisify } = require('util')
+const preaddir = promisify(readdir)
 module.exports = new Command({name: 'reload', execute: function(msg) {
-    readdirAsync('./src/commands').then(files => {
+    try {
+        out = require('child_process').execSync('git pull');
+    } catch (e) {
+        return this.master.client.rest.createMessage(msg.channel, e.message);
+    }
+    preaddir('./src/commands').then(files => {
         files.forEach(file => {
             delete require.cache[require.resolve(`../src/commands/${file}`)];
         } );
