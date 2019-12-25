e82e8e3af1213fe0ded7a45b829cf2baf8b9feaf
Jacher <jwc2002@outlook.com>
2019-12-25 22:41:28 +0000

    fix sex

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
 const Command = require('../types/Command')
-module.exports = new Command({name: 'join', execute: function(msg) {
+module.exports = new Command({name: 'join', execute: function(msg, args) {
     for (const worker of this.master.workers) {
         worker.joinVoiceChannel(msg.guild.id, args[0]);
     }
