8cc0bf4f8f13f573da96e3dd0667ee9f84dde9e2
Jacher <jwc2002@outlook.com>
2019-12-25 22:30:16 +0000

    no git pull...

--- commands/reload.js
+++ commands/reload.js
@@ -1,10 +1,5 @@
 const Command = require('../types/Command')
 module.exports = new Command({name: 'reload', execute: function(msg) {
-    try {
-        out = require('child_process').execSync('git pull');
-    } catch (e) {
-        return this.master.client.rest.createMessage(msg.channel, e.message);
-    }
     readdirAsync('./src/commands').then(files => {
         files.forEach(file => {
             delete require.cache[require.resolve(`../src/commands/${file}`)];
@@ -12,5 +7,5 @@ module.exports = new Command({name: 'reload', execute: function(msg) {
         this.commands = [];
         this.commander.registerCommands();
     } );
-    return this.master.client.rest.createMessage(msg.channel, "Rebuilt");
+    return this.master.client.rest.createMessage(msg.channel, "Reloaded");
 }});
