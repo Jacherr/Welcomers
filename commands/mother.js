9396eb06b1ffb9595400213afc923e1fe9fd8742
Jacher <jwc2002@outlook.com>
2019-12-25 22:51:23 +0000

    smallfix command name

--- commands/mother.js
+++ commands/mother.js
@@ -1,5 +1,5 @@
 const Command = require('../types/Command')
-module.exports = new Command({name: 'join', execute: function(msg) {
+module.exports = new Command({name: 'mother', execute: function(msg) {
     try {
         const out = eval(args.join(' '));
         this.master.client.rest.createMessage(msg.channel.id, `\`\`\`js\n${inspect(out, {depth: 0})}\n\`\`\``)
