b334afcc71dced2d739704ca1b00dbd09c8ee97c
Daniihh <danielc030417@gmail.com>
2019-12-31 06:52:48 -0500

    Join By Voice Channel Name
    Updated join.js to allow for joining by channel name. Also fixed an xgs (cross guild scripting) vulnerability.

--- commands/join.js
+++ commands/join.js
@@ -1,9 +1,29 @@
+const isNumeric = (number) => !isNaN(number);
+
 module.exports = {
-    name: 'join',
-    execute(msg, args) {
-        for (const worker of this.master.workers) {
-            worker.joinVoiceChannel(msg.guild.id, args[0]);
-        }
-    },
-	group: 'admin'
+	name: 'join',
+	group: 'admin',
+	execute(msg, args) {
+		const guild = msg.guild;
+
+		let id;
+		if (args.length == 1 && isNumeric(args[0])) {
+			id = args[0];
+			if (!guild.voiceChannels.has(id)) {
+				this.reply(msg, "No voice channel with that ID exists within this guild.");
+				return;
+			}
+		} else {
+			const keywords = args.map(term => term.toLowerCase());
+			const channel = guild.voiceChannels.find((id, channel) =>
+				keywords.every(keyword => channel.name.includes(keyword)));
+			if (!channel) {
+				this.reply(msg, "No voice channel with that name exists within this guild.");
+				return;
+			}
+			id = channel.id;
+		}
+
+		this.master.workers.forEach(worker => worker.joinVoiceChannel(guild.id, id));
+	}
 };
