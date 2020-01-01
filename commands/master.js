2b103b48edffea3c8c4a1b5188195beadabba8ca
Vap0r1ze <superdash993@gmail.com>
2020-01-01 01:35:30 -0500

    promise resolution

--- commands/master.js
+++ commands/master.js
@@ -1,13 +1,31 @@
 const { inspect } = require('util')
 
+function wrapCodeblock (value) {
+  return `\`\`\`js\n${inspect(value, { depth: 0 }).replace(/``/g, '``\u200b')}\n\`\`\``
+}
+
 module.exports = {
   name: 'master',
   execute (msg, args) {
     try {
-      const out = eval(args.join(' ')) // eslint-disable-line no-eval
-      this.master.client.rest.createMessage(msg.channel.id, `\`\`\`js\n${inspect(out, { depth: 0 })}\n\`\`\``)
+      const value = eval(args.join(' ')) // eslint-disable-line no-eval
+      if (value instanceof Promise) {
+        if (inspect(value) === 'Promise { <pending> }') {
+          this.master.createMessage(msg.channel.id, `**Resolving promise...**\n${wrapCodeblock(value)}`).then(masterMsg => {
+            value.then(promiseValue => {
+              masterMsg.edit(`**Promise resolved** 😎\n${wrapCodeblock(promiseValue)}`)
+            })
+          })
+        } else {
+          value.then(promiseValue => {
+            this.master.createMessage(msg.channel.id, `**Promise resolved** 😎\n${wrapCodeblock(promiseValue)}`)
+          })
+        }
+      } else {
+        this.master.createMessage(msg.channel.id, wrapCodeblock(value))
+      }
     } catch (error) {
-      this.master.client.rest.createMessage(msg.channel.id, error.message)
+      this.master.createMessage(msg.channel.id, error.message)
     }
   },
   group: 'owner'
